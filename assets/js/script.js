21c227632c087c0bd17c813cf00dbad7e96b35f2
sebastian <sebasaries15@gmail.com>
2023-06-27 00:21:28 -0500

    Partes del header terminada al 92%

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,8 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
 /* HEADER  */
 
 const header = document.querySelector("[data-header]");
+const nameFirst = document.querySelector("[data-name-first]");
+const descriptionLogo = document.querySelector("[data-description-logo]");
 
 let lastscrollPos = 0;
 
@@ -47,10 +49,77 @@ const hideHeader = function () {
 };
 
 window.addEventListener("scroll", function () {
-  if (this.window.scrollY >= 50) {
+  if (this.window.scrollY >= 75) {
     header.classList.add("active");
+    nameFirst.classList.add("hide");
+    descriptionLogo.classList.add("active");
     hideHeader();
   } else {
     header.classList.remove("active");
+    nameFirst.classList.remove("hide");
+    descriptionLogo.classList.remove("active");
   }
 });
+
+/*** HERO SLIDER ***/
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+const heroSliderNextBtn = document.querySelector("[data-next-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function () {
+  lastActiveSliderItem.classList.remove("active");
+  heroSliderItems[currentSlidePos].classList.add("active");
+  lastActiveSliderItem = heroSliderItems[currentSlidePos];
+};
+
+const slideNext = function () {
+  if (currentSlidePos >= heroSliderItems.length - 1) {
+    currentSlidePos = 0;
+  } else {
+    currentSlidePos++;
+  }
+
+  updateSliderPos();
+};
+
+heroSliderNextBtn.addEventListener("click", slideNext);
+
+const slidePrev = function () {
+  if (currentSlidePos <= 0) {
+    currentSlidePos = heroSliderItems.length - 1;
+  } else {
+    currentSlidePos--;
+  }
+  updateSliderPos();
+};
+
+heroSliderPrevBtn.addEventListener("click", slidePrev);
+
+/* AUTO SLIDE */
+let autoSlideInterval;
+
+const autoSlide = function () {
+  autoSlideInterval = setInterval(function () {
+    slideNext();
+  }, 7000);
+};
+
+addEventOnElements(
+  [heroSliderNextBtn, heroSliderPrevBtn],
+  "mouseover",
+  function () {
+    clearInterval(autoSlideInterval);
+  }
+);
+
+addEventOnElements(
+  [heroSliderNextBtn, heroSliderPrevBtn],
+  "mouseout",
+  autoSlide
+);
+
+window.addEventListener("load", autoSlide);
